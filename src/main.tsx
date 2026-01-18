8cc26254d9a10efc030543de9d3cc3e8bb7240b2
Suyog Sontakke <suyogop12@gmail.com>
2026-01-19 00:02:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <--- THIS IS THE MISSING MAGIC LINE!
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
